cc674790430163c5d7af5dcdb593710601d4d75e
JmcGrath2 <46888710+JmcGrath2@users.noreply.github.com>
2019-10-01 09:40:04 +0100

    Ex 1 and 2 completed

---
+++ Lab3/EX 1-2/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_1_2
+{
+    public class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        private double size;
+
+        public double Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine("Make : {0} \nModel : {1}\nSpeed : {2}\nEngine Size : {3}", Make, Model, Speed, Size);
+
+
+        }
+        public override string ToString()
+        {
+            return
+                $"Car Make : { Make}" + $"\nCar Model : {Model}" + $"\nCurrent Speed : {Speed}" + $"\nEngine Size : {Size}";
+        }
+        public void Accelerate()
+        {
+            Speed += 10;
+            Console.WriteLine("Current car speed is {0}", Speed);
+        }
+
+
+
+    }
+}
---
+++ Lab3/EX 1-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Ford";
+            car1.Model = "Escort";
+            car1.Speed = 55;
+            car1.Size = 2.0;
+
+            Car car2 = new Car();
+            car2.Make = "Mitsubishi";
+            car2.Model = "Lancer Evolution";
+            car2.Speed = 68;
+            car2.Size = 2.1;
+
+            Console.WriteLine("Car 1 is a {0} {1} moving at a speed of {2} miles per hour with an engine size of {3} litres", car1.Make, car1.Model, car1.Speed, car1.Size);
+            Console.WriteLine("Car 2 is a {0} {1} moving at a speed of {2} miles per hour with an engine size of {3} litres", car2.Make, car2.Model, car2.Speed, car2.Size);
+
+            car1.DisplayCarInfo();
+
+            Console.WriteLine(car2);
+
+            for(int i =0; i <10;i++)
+            {
+                car1.Accelerate();
+                car2.Accelerate();
+            }
+            Console.WriteLine(car1);
+            car2.DisplayCarInfo();
+        }
+    }
+}
